03237b421b2e2bf937bb196bbd5f86e9805b107e
aminsemer <aminsemer>
2023-11-06 00:22:33 +0000

    Avalicao tá on

---
+++ src/etl/avaliação.sql
@@ -0,0 +1,42 @@
+-- Databricks notebook source
+WITH tb_pedido AS (
+
+  SELECT DISTINCT 
+         t1.idPedido,
+         t2.idVendedor
+  FROM silver.olist.pedido AS t1
+
+    LEFT JOIN silver.olist.item_pedido AS t2 
+      ON t1.idPedido = t2.idPedido
+
+    --LEFT JOIN silver.olist.cliente AS t3 
+    --  ON t1.idCliente = t3.idCliente
+
+  WHERE t1.dtPedido <= '2018-01-01'
+    AND t1.dtPedido >= add_months('2018-01-01', -6)
+    AND idVendedor IS NOT NULL
+),
+
+tb_join AS (
+  SELECT t1.*,
+         t2.vlNota 
+  FROM tb_pedido AS t1
+    LEFT JOIN silver.olist.avaliacao_pedido AS t2 
+      ON t1.idPedido = t2.idPedido
+),
+
+  tb_summary AS (
+  SELECT idVendedor,
+         AVG(vlNota) AS avgNota,
+         percentile(vlNota, 0.5) AS medianNota,
+         min(vlNota) AS minNota,
+         max(vlNota) AS maxNota,
+         COUNT(vlNota) / COUNT(idPedido) AS pctAvaliacao ---%de pedidos que possuem avaliação
+
+  FROM tb_join
+  GROUP BY idVendedor
+  )
+
+  SELECT '2018-01-01' AS dtReference,
+          *
+  FROM tb_summary
